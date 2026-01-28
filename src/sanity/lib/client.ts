99ffe19886ab57c16c34da30da35c7ffb168c8d3
johanrussouw <jrussouw610505@gmail.com>
2026-01-28 16:39:08 +0200

    stripe webhook implemented and tested

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,7 +6,8 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  perspective: "published",
 });
 
 // Write client (for mutations - used in webhooks/server actions)
